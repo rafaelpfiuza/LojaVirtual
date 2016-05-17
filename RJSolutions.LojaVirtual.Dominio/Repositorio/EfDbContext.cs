e56cb9fddd17d9476af3d31799c0a9a76f4cb5ff
Rafael Pinto Fiúza <rafaelpfiuza@hotmail.com>
2016-05-17 09:58:04 -0300

    View Produtos

---
+++ RJSolutions.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RJSolutions.LojaVirtual.Dominio.Entidade;
+
+namespace RJSolutions.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext: DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+
+    }
+
+}
---
+++ RJSolutions.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using RJSolutions.LojaVirtual.Dominio.Entidade;
+
+namespace RJSolutions.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+        private readonly EfDbContext _context = new EfDbContext();
+
+        public IEnumerable<Produto> Produtos
+        {
+            get { return _context.Produtos; }
+        }
+
+    }
+}
