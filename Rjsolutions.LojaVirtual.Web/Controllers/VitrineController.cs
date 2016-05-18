884d40400d778d8237af41c3ccf29c4ff49af687
Rafael Pinto Fiúza <rafaelpfiuza@hotmail.com>
2016-05-18 16:37:04 -0300

    7

---
+++ Rjsolutions.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RJSolutions.LojaVirtual.Dominio.Repositorio;
+
+namespace Rjsolutions.LojaVirtual.Web.Controllers
+{
+    public class VitrineController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+        public int ProdutosPorPagina = 8;
+        // GET: Produtos
+        public ActionResult ListaProdutos(int pagina = 1)
+        {
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos.OrderBy(p => p.Produtoid)
+                .Skip((pagina - 1)*ProdutosPorPagina)
+                .Take(ProdutosPorPagina);
+            return View(produtos);
+        }
+    }
+}
